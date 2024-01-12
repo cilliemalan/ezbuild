6ae5506c635108047a1279628c6973299acf51af
Cillié Malan <cilliemalan@gmail.com>
2024-01-12 21:42:58 +0200

    Experimenting with some ts defs

---
+++ src/ezbuild.d.ts
@@ -0,0 +1,65 @@
+
+/// <reference no-default-lib="true"/>
+/// <reference lib="es2015" />
+
+interface Target {
+    type: "executable" | "module" | "staticLibrary";
+    name: string;
+    sources: string[];
+    private_include_directories: string[];
+    public_include_directories: string[];
+    private_link_directories: string[];
+    public_link_directories: string[];
+    private_compile_definitions: string[];
+    public_compile_definitions: string[];
+    private_compile_options: string[];
+    public_compile_options: string[];
+    dependencies: string[];
+
+    addPublicIncludeDir(...items: string[]): Target;
+    addPrivateIncludeDir(...items: string[]): Target;
+    addPublicLinkDir(...items: string[]): Target;
+    addPrivateLinkDir(...items: string[]): Target;
+    addPublicCompileDefinition(...items: string[]): Target;
+    addPrivateCompileDefinition(...items: string[]): Target;
+    addPublicCompileOptions(...items: string[]): Target;
+    addPrivateCompileOptions(...items: string[]): Target;
+    link(...items: (string | Target)[]): Target;
+    addSource(...files: (string | File)[]);
+}
+
+declare function addModule(name: string, files: (string | File)[]): Target;
+declare function addExecutable(name: string, files: (string | File)[]): Target;
+declare function addStaticLibrary(name: string, files: (string | File)[]): Target;
+
+interface FileSystemObject {
+    name: string;
+    localName: string;
+}
+
+interface File extends FileSystemObject {
+    size: number;
+    extension: string;
+}
+
+interface Directory extends FileSystemObject {
+    subDirectory: (name: string) => Directory;
+    subDirectories: () => Directory[];
+    subDirectoriesRecursive: () => Directory[];
+    files: () => File[];
+    filesRecursive: () => File[];
+    filesWithExtension: (...extensions: string[]) => File[];
+    filesWithExtensionRecursive: (...extensions: string[]) => File[];
+}
+
+interface Tool {
+    family: "gcc" | "clang" | "msvc" | "other";
+    type: "compiler" | "linker";
+    inputExtensions: string[];
+    outputExtension: string;
+}
+
+interface Toolchain {
+    name: string;
+    tools: Tool[];
+}
